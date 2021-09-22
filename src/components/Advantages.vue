31fe59fc2510b25ef7687d65a92441b6f4907b1a
lakub-muravlov <lakub.muravlov@gmail.com>
2021-09-21 18:46:11 -0700

    Advantages style refactor

--- src/components/Advantages.vue
+++ src/components/Advantages.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="advantages content-container" id="advantages">
-		<h1 id="advanatages">Advantages</h1>
+		<h2 id="advanatages">Advantages</h2>
 		<AdvantageItem title="Partnership">
 			We make sure projects become successful through strategic partnership and
 			guidance
@@ -33,62 +33,61 @@ export default {
 <style scoped lang="scss">
 @import "/src/assets/styles/variables.scss";
 
+@mixin line-facade {
+	content: "";
+	position: absolute;
+	background-color: #c1c1c1;
+}
+
 .advantages {
-	padding-left: 0px;
 	min-height: 200px;
 	padding-top: 70px;
 	padding-bottom: 90px;
+	padding-left: 0px;
 	display: grid;
 	justify-content: center;
 	grid-gap: 100px 15px;
 	grid-template-rows: auto auto;
 	grid-template-columns: calc(100% / 4) calc(100% / 4) calc(100% / 4);
-	h1 {
+	h2 {
 		position: relative;
 		grid-column-start: 1;
 		grid-column-end: 4;
 	}
-	h1::before {
-		content: "";
-		position: absolute;
+	h2::before {
+		@include line-facade();
 		left: 4px;
 		bottom: -110%;
 		height: 55px;
 		width: 1px;
-		background-color: #c1c1c1;
 	}
-	h1::after {
-		content: "";
-		position: absolute;
+	h2::after {
+		@include line-facade();
 		left: 4px;
 		top: -110%;
 		height: 45px;
 		width: 1px;
-		background-color: #c1c1c1;
 	}
 }
 
 .right-line {
 	position: relative;
 }
+
 .right-line::before {
-	content: "";
-	position: absolute;
+	@include line-facade();
 	left: calc(-205.3% - 10px);
 	top: 100%;
 	height: 91px;
 	width: 1.5px;
-	background-color: #c1c1c1;
 }
 
 .right-line::after {
-	content: "";
-	position: absolute;
+	@include line-facade();
 	right: -40px;
 	top: -130%;
 	height: 280%;
 	width: 1px;
-	background-color: #c1c1c1;
 }
 
 @media only screen and (max-width: $bp-laptop-s) {
@@ -98,10 +97,10 @@ export default {
 	.right-line::before {
 		display: none;
 	}
-	h1::before {
+	h2::before {
 		display: none;
 	}
-	h1::after {
+	h2::after {
 		display: none;
 	}
 }
@@ -116,11 +115,10 @@ export default {
 		flex-direction: column;
 		justify-content: flex-start;
 		align-items: flex-end;
-		h1 {
+		h2 {
 			margin-top: -75px;
 			padding-top: 20px;
 			margin-left: 0px;
-
 			align-self: flex-start;
 			font-size: 1.8em;
 		}
@@ -131,30 +129,15 @@ export default {
 	.advantages {
 		padding-left: 0px;
 		margin-left: 30px;
-
-		display: flex;
 		max-width: calc(100% - 60px);
-		flex-direction: column;
-		justify-content: flex-start;
-		align-items: flex-end;
-		h1 {
-			margin-top: -75px;
-			padding-top: 20px;
-			margin-left: 0px;
-
-			align-self: flex-start;
-			font-size: 1.8em;
-		}
-		h1::before {
+		h2::before {
 			z-index: -1;
 			display: inline;
-			content: "";
-			position: absolute;
+			@include line-facade();
 			left: 0px;
 			top: 5px;
 			height: calc(300vw + 10px);
 			width: 1px;
-			background-color: #c1c1c1;
 		}
 	}
 }
